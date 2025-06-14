06431d846ce4e8e481d2036a8998690538ad713d
Andrew Theiss <andrew.theiss@gmail.com>
2025-06-14 00:52:00 -0400

    Setup deployment for dev

--- src/utils/gasEstimation.ts
+++ src/utils/gasEstimation.ts
@@ -1,15 +1,18 @@
 // gasEstimator.ts
 import { NodeInterface__factory } from '@arbitrum/sdk/dist/lib/abi/factories/NodeInterface__factory';
 import { NODE_INTERFACE_ADDRESS } from '@arbitrum/sdk/dist/lib/dataEntities/constants';
+import { formatEther } from 'ethers';
+import { Provider } from '@ethersproject/providers';
 
 export async function estimateGasForChain(
   chainId: number,
-  provider: ,
+  provider: Provider,
   transactionDetails: any,
   nodeInterfaceAddress?: string
-): Promise<string> {
+): Promise<bigint> {
   if (chainId === 1 || chainId === 11155111) { // Ethereum mainnet or Sepolia
-    return await provider.estimateGas(transactionDetails);
+    const gasEstimate = await provider.estimateGas(transactionDetails);
+    return BigInt(gasEstimate.toString());
   } else if (chainId === 42161 || chainId === 421614) { // Arbitrum One or Sepolia
     const nodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, provider);
     const gasEstimateComponents = await nodeInterface.callStatic.gasEstimateComponents(
@@ -18,7 +21,7 @@ export async function estimateGasForChain(
       transactionDetails.data,
       { blockTag: 'latest' }
     );
-    return gasEstimateComponents.gasEstimate.toString();
+    return BigInt(gasEstimateComponents.gasEstimate.toString());
   } else if (chainId === 421613) { // Example Orbit chain ID
     const orbitNodeInterface = NodeInterface__factory.connect(nodeInterfaceAddress || '', provider);
     const gasEstimateComponents = await orbitNodeInterface.callStatic.gasEstimateComponents(
@@ -27,24 +30,26 @@ export async function estimateGasForChain(
       transactionDetails.data,
       { blockTag: 'latest' }
     );
-    return gasEstimateComponents.gasEstimate.toString();
+    return BigInt(gasEstimateComponents.gasEstimate.toString());
   } else {
     throw new Error(`Unsupported chain ID: ${chainId}`);
   }
 }
 
 export async function estimateTotalGasForMultiChain(
-  l1Provider: 
-  l2Provider: 
-  l3Provider: 
+  l1Provider: Provider,
+  l2Provider: Provider,
+  l3Provider: Provider,
   l1ToL2Transaction: any,
   l2ToL3Transaction: any
 ): Promise<string> {
   const l1GasEstimate = await estimateGasForChain(1, l1Provider, l1ToL2Transaction);
-  const l1GasCost = utils.parseUnits(l1GasEstimate, 'wei').mul(await l1Provider.getGasPrice());
+  const l1GasPrice = await l1Provider.getFeeData();
+  const l1GasCost = l1GasEstimate * BigInt(l1GasPrice.gasPrice?.toString() || '0');
 
   const l2GasEstimate = await estimateGasForChain(42161, l2Provider, l2ToL3Transaction);
-  const l2GasCost = utils.parseUnits(l2GasEstimate, 'wei').mul(await l2Provider.getGasPrice());
+  const l2GasPrice = await l2Provider.getFeeData();
+  const l2GasCost = l2GasEstimate * BigInt(l2GasPrice.gasPrice?.toString() || '0');
 
   const l1NodeInterface = NodeInterface__factory.connect(NODE_INTERFACE_ADDRESS, l1Provider);
   const l1GasComponents = await l1NodeInterface.callStatic.gasEstimateComponents(
@@ -53,8 +58,8 @@ export async function estimateTotalGasForMultiChain(
     l1ToL2Transaction.data,
     { blockTag: 'latest' }
   );
-  const maxSubmissionCost = l1GasComponents.gasEstimateForL1.mul(l1GasComponents.l1BaseFeeEstimate);
+  const maxSubmissionCost = BigInt(l1GasComponents.gasEstimateForL1.toString()) * BigInt(l1GasComponents.l1BaseFeeEstimate.toString());
 
-  const totalEthNeeded = l1GasCost.add(maxSubmissionCost).add(l2GasCost);
-  return utils.formatEther(totalEthNeeded);
+  const totalEthNeeded = l1GasCost + maxSubmissionCost + l2GasCost;
+  return formatEther(totalEthNeeded);
 }
